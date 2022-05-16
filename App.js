1a04d57c70bf09ec206293aafa1a4cc1c2e2fc6f
carl-vanman <vanman.carl@gmail.com>
2022-05-16 14:25:37 +0200

    Added and implemented custom fonts to project

--- App.js
+++ App.js
@@ -1,20 +1,62 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useCallback, useEffect, useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+import { StatusBar } from "expo-status-bar";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+
+const customFonts = {
+    "SFProDisplay-Bold": require("./assets/fonts/SFProDisplay-Bold.otf"),
+    "SFProDisplay-Medium": require("./assets/fonts/SFProDisplay-Medium.otf"),
+    "SFProDisplay-Regular": require("./assets/fonts/SFProDisplay-Regular.otf"),
+};
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    const [appIsReady, setAppIsReady] = useState(false);
+
+    useEffect(() => {
+        async function prepare() {
+            try {
+                await SplashScreen.preventAutoHideAsync();
+                await Font.loadAsync(customFonts);
+            } catch (e) {
+                console.warn(e);
+            } finally {
+                setAppIsReady(true);
+            }
+        }
+        prepare();
+    }, []);
+
+    const onLayoutRootView = useCallback(async () => {
+        if (appIsReady) {
+            await SplashScreen.hideAsync();
+        }
+    }, [appIsReady]);
+
+    if (!appIsReady) {
+        return null;
+    }
+
+    return (
+        <View style={styles.container} onLayout={onLayoutRootView}>
+            <Text style={styles.text}>
+                Open up App.js to start working on your app!
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
+    text: {
+        fontFamily: "SFProDisplay-Bold",
+        color: "red",
+    },
 });
