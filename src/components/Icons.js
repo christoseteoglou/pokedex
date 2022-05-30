0f25911200d3d617a2fcdc07fdee30d0b8419fe3
Christos Eteoglou <eatoldglue@Christoss-MacBook-Pro.local>
2022-05-30 12:21:15 +0200

    Icons and IconSets

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -2,7 +2,7 @@ import { StyleSheet, Pressable } from 'react-native'
 import React from 'react'
 
 
-import { Grass, Normal, Fire, Dragon,Poison, Bug, Water, Flying, Dark, Electric, Fairy, Fighting, Ghost, Ground, Rock } from '../components/iconSet'
+import { Grass, Normal, Fire, Dragon,Poison, Bug, Water, Flying, Dark, Electric, Fairy, Fighting, Ghost, Ground, Rock, Steel, Psychic, Ice } from '../components/iconSet'
 import { textColor } from '../../assets/colors'
 
 
@@ -16,42 +16,51 @@ const Icons = ({ children, onPress, name, style, width, height}) => {
       case 'fire':
       RenderIcon = Fire;
       break;
-      case 'dragon':
+    case 'dragon':
       RenderIcon = Dragon;
       break;
-      case 'poison':
-        RenderIcon = Poison;
-        break;
-      case 'bug':
-        RenderIcon = Bug;
-        break;
-      case 'water':
-        RenderIcon = Water;
-        break;
-      case 'flying':
-        RenderIcon = Flying;
-        break;
-        case 'normal':
-          RenderIcon = Normal;
-          break;
-        case 'electric':
-          RenderIcon = Electric;
-          break;
-        case 'fairy':
-        RenderIcon = Fairy;
-        break;
-        case 'fighting':
-        RenderIcon = Fighting;
-        break;
-        case 'ghost':
-        RenderIcon = Ghost;
-        break;
-        case 'ground':
-        RenderIcon = Ground;
-        break;
-        case 'rock':
-          RenderIcon = Rock;
-          break;
+    case 'poison':
+      RenderIcon = Poison;
+      break;
+    case 'bug':
+      RenderIcon = Bug;
+      break;
+    case 'water':
+      RenderIcon = Water;
+      break;
+    case 'flying':
+      RenderIcon = Flying;
+      break;
+    case 'normal':
+      RenderIcon = Normal;
+      break;
+    case 'electric':
+      RenderIcon = Electric;
+      break;
+    case 'fairy':
+      RenderIcon = Fairy;
+      break;
+    case 'fighting':
+      RenderIcon = Fighting;
+      break;
+    case 'ghost':
+      RenderIcon = Ghost;
+      break;
+    case 'ground':
+      RenderIcon = Ground;
+      break;
+    case 'rock':
+      RenderIcon = Rock;
+      break;
+    case 'steel':
+      RenderIcon = Steel;
+      break;
+    case 'psychic':
+      RenderIcon = Psychic;
+      break;
+    case 'ice':
+      RenderIcon = Ice;
+      break;
     default:
       RenderIcon = Dark;
   }
